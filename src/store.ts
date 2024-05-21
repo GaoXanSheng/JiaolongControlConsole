511af064021f775183bd11911eb477eaf3e78e9e
Gaoxusheng <moxiaojulove@foxmail.com>
2024-05-21 22:45:36 +0800

    fix store async

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,6 @@
 import {createStore} from 'vuex';
 import wmiOperation from "./WMIOperation/WMIOperation.ts";
-import {eumCPUPower, eumPerformaceMode, ResultState} from "./WMIOperation/Models/IPCModels.ts";
+import {eumCPUPower, eumGPUMode, eumPerformaceMode, ResultState} from "./WMIOperation/Models/IPCModels.ts";
 
 export enum HomeTab {
     HOME,
@@ -11,53 +11,62 @@ export enum HomeTab {
     Keyboard
 }
 
-
-const storeModels = {
-    state: {
-        SwitchPages: HomeTab.HOME,
-        CpuPage: {
-            longPower: 45,
-            tempWall: 80,
-            shortPower: 45,
-        },
-        FanPage: {
-            isTrue: false,
-            SetFanSpeed: 3500,
-        },
-        OS: {
-            PerformaceMode:eumPerformaceMode.Unknow,
-            LogoLight:(await wmiOperation.GetLogoLight()),
-            Customize:eumCPUPower.Unknow,
-            SwitchMaxFanSpeed:ResultState.OFF,
-            CPU: {
-                temperature:0,
-                CPUFanSpeed:0,
-                tempWall:0
+function initStore() {
+    const storeModels =   {
+        state: {
+            SwitchPages: HomeTab.HOME,
+            CpuPage: {
+                longPower: 45,
+                tempWall: 80,
+                shortPower: 45,
             },
-            GPU: {
-                temperature:0,
-                GPUFanSpeed:0,
-                GpuMode:( await wmiOperation.Gpu.GetGpuMode())
+            FanPage: {
+                isTrue: false,
+                SetFanSpeed: 3500,
+            },
+            OS: {
+                PerformaceMode:eumPerformaceMode.Unknow,
+                LogoLight:"",
+                Customize:eumCPUPower.Unknow,
+                SwitchMaxFanSpeed:ResultState.OFF,
+                CPU: {
+                    temperature:0,
+                    CPUFanSpeed:0,
+                    tempWall:0
+                },
+                GPU: {
+                    temperature:0,
+                    GPUFanSpeed:0,
+                    GpuMode:eumGPUMode.Unknow
+                }
+            },
+            EventLoopPage:{
+                isRun:false,
+                eventLoop:null,
+                STC: [
+                    [35, 100],[45, 90],[55, 80],[65, 70],[75, 50],[85, 40],[95, 30],[100, 20],[110, 10],[120, 0]
+                ],
+                // Temperature long-term power consumption scheduling
+                LTC:[
+                    [35, 100],[45, 90],[55, 80],[65, 70],[75, 50],[85, 40],[95, 30],[100, 20],[110, 10],[120, 0]
+                ],
+                // Temperature and speed
+                TFS:[
+                    [3500, 60],[3600, 65],[3700, 70],[3800, 75],[4000, 80],[4500, 90],[4800, 95],[5000, 98],[5500, 99],[5800, 100]
+                ]
             }
         },
-        EventLoopPage:{
-            isRun:false,
-            eventLoop:null,
-            STC: [
-                [35, 100],[45, 90],[55, 80],[65, 70],[75, 50],[85, 40],[95, 30],[100, 20],[110, 10],[120, 0]
-            ],
-            // Temperature long-term power consumption scheduling
-            LTC:[
-                [35, 100],[45, 90],[55, 80],[65, 70],[75, 50],[85, 40],[95, 30],[100, 20],[110, 10],[120, 0]
-            ],
-            // Temperature and speed
-            TFS:[
-                [3500, 60],[3600, 65],[3700, 70],[3800, 75],[4000, 80],[4500, 90],[4800, 95],[5000, 98],[5500, 99],[5800, 100]
-            ]
-        }
-    },
+    }
+    wmiOperation.GetLogoLight().then(r=>{
+        storeModels.state.OS.LogoLight = r
+    }).catch(e=>alert("运行时出现异常，请报告错误"))
+    wmiOperation.Gpu.GetGpuMode().then(r=>{
+        storeModels.state.OS.GPU.GpuMode = r
+    }).catch(e=>alert("运行时出现异常，请报告错误"))
+    return storeModels
 }
-const store = createStore(storeModels);
+
+const store = createStore(initStore());
 setInterval(async () => {
     const {gpuTemp,cpuTemp} = await wmiOperation.System.GetInfo()
     const {CPUFanSpeed,GPUFanSpeed} =  await wmiOperation.Fan.GetFanSpeed()
