8a57298a76f48c57b308ec7c6452999aad80461d
Gaoxusheng <moxiaojulove@foxmail.com>
2024-05-22 21:03:37 +0800

    fix BuildCopyExeFile bugs remove adm-zip

--- BuildCopyExeFile.js
+++ BuildCopyExeFile.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const path = require("node:path");
 const {exec} = require("child_process");
-const AdmZip = require('adm-zip');
-
 const sourceDir = path.join(__dirname, 'JiaoLongWMI');
 const destDir = path.join(__dirname, 'release', '0.0.0', 'win-unpacked', 'resources','JiaoLongWMI');
 const delFileDir = path.join(__dirname, 'release', '0.0.0');
