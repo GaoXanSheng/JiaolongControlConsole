2b995d125230b5f39bb3889917690dbdf3d30c4a
Gaoxusheng <moxiaojulove@foxmail.com>
2024-08-04 21:39:39 +0800

    fix Quitting bugs

--- src/main/tools/tray.ts
+++ src/main/tools/tray.ts
@@ -1,30 +1,34 @@
-import {app, BrowserWindow, Menu, Tray} from "electron";
+import { app, BrowserWindow, Menu, Tray } from 'electron'
 
-export default function (win: BrowserWindow,icon:string) {
-    const tray = new Tray(icon); // 请确保这里有一个图标文件
-    const contextMenu = Menu.buildFromTemplate([
-        {
-            label: '显示界面',
-            click: function () {
-                win.show();
-            }
-        },
-        {
-            label: '退出',
-            click: function () {
-                app.quit();
-            }
-        }
-    ]);
+export default function(win: BrowserWindow, icon: string) {
+  let isQuitting = false
+  const tray = new Tray(icon) // 请确保这里有一个图标文件
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: '显示界面',
+      click: function() {
+        win.show()
+      }
+    },
+    {
+      label: '退出',
+      click: function() {
+        isQuitting = true
+        app.quit()
+      }
+    }
+  ])
 
-    tray.setToolTip('JiaolongControlConsole');
-    tray.on('click', () => {
-        win.show();
-    })
-    tray.setContextMenu(contextMenu);
-    // 最小化到托盘
-    win.on('close', function (event) {
-        event.preventDefault();
-        win.hide();
-    });
+  tray.setToolTip('JiaolongControlConsole')
+  tray.on('click', () => {
+    win.show()
+  })
+  tray.setContextMenu(contextMenu)
+  // 最小化到托盘
+  win.on('close', function(event) {
+    if (!isQuitting) {
+      event.preventDefault()
+      win.hide()
+    }
+  })
 }
